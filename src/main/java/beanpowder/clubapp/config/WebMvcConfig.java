070787f706a67eb8cf75458c5c394c2c936e3b98
itsfrankocean <76962574+itsfrankocean@users.noreply.github.com>
2024-11-16 12:31:50 +0900

    Update WebMvcConfig.java

--- src/main/java/beanpowder/clubapp/config/WebMvcConfig.java
+++ src/main/java/beanpowder/clubapp/config/WebMvcConfig.java
@@ -14,7 +14,7 @@ public class WebMvcConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // 모든 엔드포인트에 대해 CORS 허용
-                        .allowedOrigins("http://127.0.0.1:3000", "http://localhost:3000") // 허용할 프론트엔드 도메인
+                        .allowedOrigins("*") // 허용할 프론트엔드 도메인
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
                         .allowedHeaders("*") // 모든 헤더 허용
                         .allowCredentials(true); // 쿠키 및 인증 정보 허용
